6701804115eb139b390871168de1030531c221c5
Spencer Bray <spencerjohnbray@gmail.com>
2017-07-10 15:55:17 +0200

    add api to rails config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,23 @@ class ApplicationController < ActionController::Base
     render json: { message: "Wrong content-type header" }, status: 406 and return unless request.content_type == "application/json"
   end
 
+  # This error rescue code was from a module we did on API's in school, but it looks like we can just use rails config
+  # rescue_from ActiveRecord::RecordNotFound, with: :not_found
+  # rescue_from StandardError, with: :internal_server_error
+
+  # private
+
+  # def not_found
+  #   render json: { error: exception.message }, status: :not_found
+  # end
+
+  # def internal_server_error(exception)
+  #   if Rails.env.development?
+  #     response = { type: exception.class.to_s, error: exception.message }
+  #   else
+  #     response = { error: "Internal Server Error" }
+  #   end
+  #   render json: response, status: :internal_server_error
+  # end
+
 end
